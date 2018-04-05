2169370779262ee4740e8998d4449e7d3b9b88d3
Maurice de Laat <info@mauricedelaat.nl>
2018-04-05 20:04:59 +0200

    Updated @yellicode/templating dependency for TypeNameProvider changes.

--- src/typescript-type-name-provider.ts
+++ src/typescript-type-name-provider.ts
@@ -2,13 +2,13 @@
 import { DefaultTypeNameProvider } from '@yellicode/templating';
 
 export class TypeScriptTypeNameProvider extends DefaultTypeNameProvider {
-    protected /*override*/ getPrimitiveTypeName(type: model.PrimitiveType): string | null {
+    protected /*override*/ getDataTypeName(type: model.DataType): string | null {
         if (model.isPrimitiveBoolean(type)) return "boolean";
         if (model.isPrimitiveInteger(type) || model.isPrimitiveReal(type))
             return "number";
 
         if (model.isPrimitiveString(type)) return "string";
         if (model.isPrimitiveObject(type)) return "any";
-        return super.getPrimitiveTypeName(type);
+        return super.getDataTypeName(type);
     }
 }
