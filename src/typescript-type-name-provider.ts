b0ea5c9840d69b39227c8a59ff86d5574d860ee6
Maurice de Laat <info@mauricedelaat.nl>
2018-12-07 13:15:54 +0100

    Implemented new DefaultTypeNameProvider functions (@yellicode/templating 1.1.0).

--- src/typescript-type-name-provider.ts
+++ src/typescript-type-name-provider.ts
@@ -2,17 +2,17 @@
 import { DefaultTypeNameProvider } from '@yellicode/templating';
 
 export class TypeScriptTypeNameProvider extends DefaultTypeNameProvider {
-    
-    protected /*override*/ getDataTypeNameForType(type: elements.Type | null): string | null {
-        if (!type)
+
+    protected /*override*/ getTypeNameForType(type: elements.Type | null, isDataType: boolean): string | null {
+        if (!type || !isDataType)
             return null;
-        
+
         if (elements.isPrimitiveBoolean(type)) return "boolean";
         if (elements.isPrimitiveInteger(type) || elements.isPrimitiveReal(type))
             return "number";
         if (elements.isPrimitiveString(type)) return "string";
         if (elements.isPrimitiveObject(type)) return "any";
 
-        return super.getDataTypeNameForType(type);
+        return super.getTypeNameForType(type, isDataType);
     }
 }
