038ea7bec6fd727751a302c38d2a726a31e05701
Maurice de Laat <info@mauricedelaat.nl>
2018-12-07 13:50:14 +0100

    TypeScriptTypeNameProvider was ignoring complex types.

--- src/typescript-type-name-provider.ts
+++ src/typescript-type-name-provider.ts
@@ -4,14 +4,16 @@ import { DefaultTypeNameProvider } from '@yellicode/templating';
 export class TypeScriptTypeNameProvider extends DefaultTypeNameProvider {
 
     protected /*override*/ getTypeNameForType(type: elements.Type | null, isDataType: boolean): string | null {
-        if (!type || !isDataType)
+        if (!type)
             return null;
 
-        if (elements.isPrimitiveBoolean(type)) return "boolean";
-        if (elements.isPrimitiveInteger(type) || elements.isPrimitiveReal(type))
-            return "number";
-        if (elements.isPrimitiveString(type)) return "string";
-        if (elements.isPrimitiveObject(type)) return "any";
+        if (isDataType) {
+            if (elements.isPrimitiveBoolean(type)) return "boolean";
+            if (elements.isPrimitiveInteger(type) || elements.isPrimitiveReal(type))
+                return "number";
+            if (elements.isPrimitiveString(type)) return "string";
+            if (elements.isPrimitiveObject(type)) return "any";
+        }
 
         return super.getTypeNameForType(type, isDataType);
     }
